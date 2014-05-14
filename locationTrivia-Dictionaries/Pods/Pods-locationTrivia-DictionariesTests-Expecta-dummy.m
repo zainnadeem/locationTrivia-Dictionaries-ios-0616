5a857d8a98ce31461ef40448a009d9b468f4eaed
Joe Burgess <joe@flatironschool.com>
2014-05-14 09:46:05 -0400

    Adding blank proj with pods

---
+++ locationTrivia-Dictionaries/Pods/Pods-locationTrivia-DictionariesTests-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_locationTrivia_DictionariesTests_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_locationTrivia_DictionariesTests_Expecta
+@end
---
+++ locationTrivia-Dictionaries/locationTrivia-Dictionaries/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  locationTrivia-Dictionaries
+//
+//  Created by Joe Burgess on 5/14/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
