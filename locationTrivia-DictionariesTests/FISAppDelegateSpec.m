60411d5567ec171c2e6726f5cc285556261f8043
markedwardmurray <markedwardmurray@gmail.com>
2015-07-13 15:26:09 -0400

    first draft

--- locationTrivia-DictionariesTests/FISAppDelegateSpec.m
+++ locationTrivia-DictionariesTests/FISAppDelegateSpec.m
@@ -10,159 +10,140 @@
 #define EXP_SHORTHAND
 #import <Expecta.h>
 #import "FISAppDelegate.h"
-#import <asl.h>
 
 
 SpecBegin(FISAppDelegate)
 
 describe(@"FISAppDelegate", ^{
     __block FISAppDelegate *appDelegate;
-    __block NSDictionary *location;
+    
+    __block NSString *empireStateName;
+    __block NSNumber *empireStateLat;
+    __block NSNumber *empireStateLng;
+    
+    __block NSDictionary *location0;
+    __block NSDictionary *location1;
     __block NSDictionary *location2;
-    __block NSDictionary *location3;
-    __block NSArray *locationsArray;
-    beforeAll(^{
-
-        appDelegate = [[FISAppDelegate alloc] init];
-        location = @{@"name":@"Empire State Building",
-                     @"latitude":@23.432,
-                     @"longitude":@-34.34
-                     };
-        location2 = @{@"name":@"The Flatiron School",
-                                    @"latitude":@34.432,
-                                    @"longitude":@-23.67
-                                    };
-        location3 = @{@"name":@"Statue Of Liberty",
-                                    @"latitude":@78.34,
-                                    @"longitude":@92.42
-                                    };
-        locationsArray = @[location,location2,location3];
-    });
+    
+    __block NSArray *locations;
     
     beforeEach(^{
 
+        appDelegate = [[FISAppDelegate alloc] init];
+        
+        empireStateName = @"Empire State Building";
+        empireStateLat  = @23.432;
+        empireStateLng  = @-34.34;
+        
+        location0 = @{ @"name"      : empireStateName,
+                       @"latitude"  : empireStateLat ,
+                       @"longitude" : empireStateLng
+                      };
+        
+        location1 = @{ @"name"      : @"The Flatiron School",
+                       @"latitude"  : @34.432 ,
+                       @"longitude" : @-23.67
+                      };
+        
+        location2 = @{ @"name"      : @"Statue Of Liberty",
+                       @"latitude"  : @78.34 ,
+                       @"longitude" : @92.42
+                      };
+        
+        locations = @[ location0, location1, location2 ];
     });
 
-    describe(@"Shorten Location Name", ^{
-        it(@"Should have a method called shortenLocationNameWithLocation:toCount:", ^{
-            expect(appDelegate).to.respondTo(@selector(shortenLocationNameWithLocation:ToCount:));
+    describe(@"shortenedNameOfLocation:toLength:", ^{
+        it(@"returns an NSString object", ^{
+            expect([appDelegate shortenedNameOfLocation:location1 toLength:5]).to.beKindOf([NSString class]);
+        });
+        
+        it(@"returns a shortened location name to 3 characters long when submitted a length argument of 3", ^{
+            NSString *shortenedName =[appDelegate shortenedNameOfLocation:location0 toLength:3];
+            expect(shortenedName.length).to.equal(3);
         });
 
-        it(@"Should return an NSString", ^{
-
-            expect([appDelegate shortenLocationNameWithLocation:location ToCount:5]).to.beKindOf([NSString class]);
-
-            NSMutableArray *consoleLog = [NSMutableArray array];
-
-            aslclient client = asl_open(NULL, NULL, ASL_OPT_STDERR);
-
-            aslmsg query = asl_new(ASL_TYPE_QUERY);
-            asl_set_query(query, ASL_KEY_MSG, NULL, ASL_QUERY_OP_NOT_EQUAL);
-            aslresponse response = asl_search(client, query);
-
-            asl_free(query);
-
-            aslmsg message;
-            while((message = aslresponse_next(response)))
-            {
-                const char *msg = asl_get(message, ASL_KEY_MSG);
-                [consoleLog addObject:[NSString stringWithFormat:@"%s" , msg]];
-            }
-            NSLog(@"%@",consoleLog);
+        it(@"returns a shortened location name to 5 characters long when submitted a length argument of 5", ^{
+            NSString *shortenedName =[appDelegate shortenedNameOfLocation:location0 toLength:5];
+            expect(shortenedName.length).to.equal(5);
         });
 
-        it(@"Should shorten Location Name to 5 characters", ^{
-            NSString *shortenedName =[appDelegate shortenLocationNameWithLocation:location ToCount:5];
-            expect([shortenedName length]).to.equal(5);
-        });
-
-        it(@"Should handle negative numbers by returning the same string", ^{
-            NSString *shortenedName =[appDelegate shortenLocationNameWithLocation:location ToCount:-5];
-            expect(shortenedName).to.equal(location[@"name"]);
-        });
-    });
-
-    describe(@"Create Location NSDictionary", ^{
-        it(@"Should have a method called createLocationWithName:Latitude:Longitude", ^{
-            expect(appDelegate).to.respondTo(@selector(createLocationWithName:Latitude:Longitude:));
-        });
-
-        it(@"Should create an appropriate NSDictionary", ^{
-            NSDictionary *returnLocation = [appDelegate createLocationWithName:@"The Flatiron School" Latitude:@34.432 Longitude:@-23.67];
-            expect(returnLocation).to.equal(location2);
-        });
-
-        it(@"should return an NSDictionary", ^{
-            expect([appDelegate createLocationWithName:@"The Flatiron School" Latitude:@34.432 Longitude:@-23.67]).to.beKindOf([NSDictionary class]);
+        it(@"returns the original location name when submitted a negative length argument", ^{
+            NSString *shortenedName =[appDelegate shortenedNameOfLocation:location0 toLength:-5];
+            expect(shortenedName).to.equal(location0[@"name"]);
         });
     });
 
-    describe(@"Get Location Names With Location", ^{
-        it(@"Should have a method called getLocationNamesWithLocations", ^{
-            expect(appDelegate).to.respondTo(@selector(getLocationNamesWithLocations:));
+    describe(@"dictionaryForLocationWithName:latitude:longitude:", ^{
+        it(@"returns an NSDictionary object", ^{
+            expect([appDelegate dictionaryForLocationWithName:empireStateName latitude:empireStateLat longitude:empireStateLng]).to.beKindOf([NSDictionary class]);
         });
 
-        it(@"Should get the locations", ^{
-            NSArray *locationNames = [appDelegate getLocationNamesWithLocations:locationsArray];
-            expect(locationNames).to.contain(location[@"name"]);
+        it(@"returns a dictionary with the submitted values for the correct keys", ^{
+            NSDictionary *newLocation = [appDelegate dictionaryForLocationWithName:empireStateName latitude:empireStateLat longitude:empireStateLng];
+            expect(newLocation).to.equal(location0);
+        });
+    });
+
+    describe(@"namesOfLocations:", ^{
+        it(@"returns an NSArray object", ^{
+            expect([appDelegate namesOfLocations:locations]).to.beKindOf([NSArray class]);
+        });
+        
+        it(@"returns an array containing the expected three objects", ^{
+            NSArray *locationNames = [appDelegate namesOfLocations:locations];
+            
+            expect(locationNames.count).to.equal(3);
+        });
+
+        it(@"returns an array containing all of the expected location names", ^{
+            NSArray *locationNames = [appDelegate namesOfLocations:locations];
+            
+            expect(locationNames).to.contain(location0[@"name"]);
+            expect(locationNames).to.contain(location1[@"name"]);
             expect(locationNames).to.contain(location2[@"name"]);
-            expect(locationNames).to.contain(location3[@"name"]);
-        });
-
-        it(@"Should return an NSArray", ^{
-            expect([appDelegate getLocationNamesWithLocations:locationsArray]).to.beKindOf([NSArray class]);
         });
     });
 
-    describe(@"Verify Location", ^{
-        it(@"Should have a method called verifyLocation", ^{
-            expect(appDelegate).to.respondTo(@selector(verifyLocation:));
-        });
-
-        it(@"Should return YES for a correct location", ^{
-            expect([appDelegate verifyLocation:location3]).to.beTruthy();
+    describe(@"dictionaryHasValidLocation:", ^{
+        it(@"returns YES for a correct location", ^{
+            expect([appDelegate dictionaryHasValidLocation:location0]).to.beTruthy();
         });
 
         it(@"Should return NO for a location with incorrect keys", ^{
-            NSDictionary *badLocation = @{@"asdf":@"asdf",
-                                          @"asdasdff":@"asdf",
-                                          @"iwir":@"asdf"};
-            expect([appDelegate verifyLocation:badLocation] ).to.beFalsy();
+            NSDictionary *locationWithBadKeys = @{ @"n4m3"     : @"Atlantis" ,
+                                                   @"l0c4710n" : @1          ,
+                                                   @"l471700d" : @1          };
+            expect([appDelegate dictionaryHasValidLocation:locationWithBadKeys]).to.beFalsy();
         });
 
         it(@"Should return NO for a location with not enough keys", ^{
-            NSDictionary *badLocation = @{@"name":@"asdf",
-                                          @"latitude":@"asdf"
-                                          };
-            expect([appDelegate verifyLocation:badLocation] ).to.beFalsy();
+            NSDictionary *locationWithNotEnoughKeys = @{ @"name"    : @"Atlantis" ,
+                                                         @"latitude": @1          };
+            
+            expect([appDelegate dictionaryHasValidLocation:locationWithNotEnoughKeys]).to.beFalsy();
         });
 
         it(@"Should return NO for a location with extra keys", ^{
-            NSDictionary *badLocation = @{@"name":@"asdf",
-                                          @"latitude":@"asdf",
-                                          @"longitude":@"asf",
-                                          @"otherThing":@"aiwef"};
-            expect([appDelegate verifyLocation:badLocation] ).to.beFalsy();
+            NSDictionary *locationWithTooManyKeys = @{ @"name"       : @"Atlantis"    ,
+                                                       @"latitude"   : @1             ,
+                                                       @"longitude"  : @1             ,
+                                                       @"trivia"     : @[ @"legend" ] };
+            expect([appDelegate dictionaryHasValidLocation:locationWithTooManyKeys]).to.beFalsy();
         });
     });
 
-    describe(@"Search for Location Name", ^{
-        it(@"Should have a method called searchForLocationName:inLocations:", ^{
-            expect(appDelegate).to.respondTo(@selector(searchForLocationName:inLocations:));
+    describe(@"retrieveLocationByName:inLocations:", ^{
+        it(@"returns an NSDictionary object", ^{
+            expect([appDelegate retrieveLocationByName:@"The Flatiron School" inLocations:locations]).to.beKindOf([NSDictionary class]);
         });
 
-        it(@"Should return an NSDictionary", ^{
-            expect([appDelegate searchForLocationName:@"The Flatiron School" inLocations:locationsArray]).to.beKindOf([NSDictionary class]);
+        it(@"returns the correct NSDictionary Location", ^{
+            expect([appDelegate retrieveLocationByName:empireStateName inLocations:locations]).to.equal(location0);
         });
 
-        it(@"Should return the appropriate NSDictionary Location", ^{
-            NSDictionary *returnedLocation = [appDelegate searchForLocationName:@"The Flatiron School" inLocations:locationsArray];
-            expect(returnedLocation).to.equal(location2);
-        });
-
-        it(@"Should return nil if no location is found", ^{
-            NSDictionary *returnedLocation = [appDelegate searchForLocationName:@"BAD LOCATION" inLocations:locationsArray];
-            expect(returnedLocation).to.beNil();
+        it(@"returns 'nil' if no location is found with that name", ^{
+            expect([appDelegate retrieveLocationByName:@"Atlantis" inLocations:locations]).to.beNil();
         });
     });
    
