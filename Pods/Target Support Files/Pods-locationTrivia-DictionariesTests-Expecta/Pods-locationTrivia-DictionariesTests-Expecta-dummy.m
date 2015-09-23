04b3f5f97cb80c67f8e6458a812f21b6a5549cae
markedwardmurray <markedwardmurray@gmail.com>
2015-09-23 15:30:26 -0400

    pods: specta 1.0.4 expecta 1.0.3

--- Pods/Target Support Files/Pods-locationTrivia-DictionariesTests-Expecta/Pods-locationTrivia-DictionariesTests-Expecta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_locationTrivia_DictionariesTests_Expecta : NSObject
-@end
-@implementation PodsDummy_Pods_locationTrivia_DictionariesTests_Expecta
-@end
